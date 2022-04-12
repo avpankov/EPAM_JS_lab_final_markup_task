66766fbfe24ffe1ba6eec574593a9b7301c784da
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2022-04-12 11:10:47 +0300

    Added changes

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 let menu = document.getElementById('hamburger-menu');
-let nav = document.getElementById('header__navigation__items');
+let nav = document.getElementById('header__navigation');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('hamburger-menu_rotate');
 
     if (nav.className == '') {
-        nav.classList.add('header__navigation__items_open');
+        nav.classList.add('header__navigation_open');
         nav.style.opacity = '1';
-    } else if (nav.className == 'header__navigation__items_open') {
-        nav.classList.add('header__navigation__items_close');
-        nav.classList.remove('header__navigation__items_open');
+    } else if (nav.className == 'header__navigation_open') {
+        nav.classList.add('header__navigation_close');
+        nav.classList.remove('header__navigation_open');
         nav.style.opacity = '0';
-    } else if (nav.className == 'header__navigation__items_close') {
-        nav.classList.add('header__navigation__items_open');
-        nav.classList.remove('header__navigation__items_close');
+    } else if (nav.className == 'header__navigation_close') {
+        nav.classList.add('header__navigation_open');
+        nav.classList.remove('header__navigation_close');
         nav.style.opacity = '1';
-    } else if (nav.className == 'header__navigation__items_close') {
-        nav.classList.add('header__navigation__items_open');
+    } else if (nav.className == 'header__navigation_close') {
+        nav.classList.add('header__navigation_open');
         nav.style.opacity = '1';
     }
 })
